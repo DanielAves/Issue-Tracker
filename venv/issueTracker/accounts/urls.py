2bfbbfc0345b79451abba0f952e5eeb18a4d52a5
Daniel Aves <sc16da@leeds.ac.uk>
2020-05-26 22:07:14 +0100

    Started implementing side navbar for site

--- venv/issueTracker/accounts/urls.py
+++ venv/issueTracker/accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home), 
+    path('users/', views.users)
 ]
--- venv/issueTracker/accounts/views.py
+++ venv/issueTracker/accounts/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'accounts/dashboard.html')
 
+def users(request):
+    return render(request, 'accounts/users.html')
